8b95d572593d606b3e5f786743855e906f5c1e9c
Han <1655466387@qq.com>
2025-05-06 16:00:08 +0800

    优化✅

--- src/App.vue
+++ src/App.vue
@@ -14,8 +14,9 @@
         theme="success"
         message="物理合并，直接将 图片合并成一张图片，无需服务器，不怕被篡改，可打印使用，稳定，绿色，安全！"
       />
-      <t-alert theme="info" message="开源地址：">
-        <template #operation>
+      <t-alert theme="info">
+        <template #message>
+          开源地址：
           <t-link
             theme="primary"
             underline
@@ -23,6 +24,10 @@
             target="_blank"
           >
             PayQrcode
+          </t-link>
+          ，技术支持：
+          <t-link theme="primary" underline href="https://www.vvhan.com" target="_blank">
+            韩小韩博客
           </t-link>
         </template>
       </t-alert>
